fe620ae6dce63a724ca1b66ee30036554464a6f4
kenloughery <64917859+kenloughery@users.noreply.github.com>
2020-09-19 13:01:24 -0600

    Added more spacing

--- Converter.java
+++ Converter.java
@@ -9,10 +9,12 @@ public class Converter {
          * Aaron Joseph
          *
          */
+        
         private double celsiusToFahrenheit(double C){
 // TODO: The third student will implement this method
             return (C*(9/5))+32;
         }
+
         private double fahrenheitToCelsius(double F){
             // TODO: The second student will implement this method
             return ((F-32)*5)/9;
@@ -21,17 +23,18 @@ public class Converter {
         private double kilometersToMiles(double K){
                 return (K * 0.6213711923);
         }
+
         public static void main(String[] args) {
-//TODO: The first student will implement this method.
+                //TODO: The first student will implement this method.
 
-// Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
-        Converter myConverter = new Converter();
-        double temperature = myConverter.celsiusToFahrenheit(180);
-        System.out.println("180 degrees Celsius is " + temperature + " degrees Fahrenheit.");
+                // Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
+                Converter myConverter = new Converter();
+                double temperature = myConverter.celsiusToFahrenheit(180);
+                System.out.println("180 degrees Celsius is " + temperature + " degrees Fahrenheit.");
 
-// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
-        temperature = myConverter.fahrenheitToCelsius(250);
-        System.out.println("250 degrees Fahrenheit is " + temperature + " degrees Celsius.");
+                // Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
+                temperature = myConverter.fahrenheitToCelsius(250);
+                System.out.println("250 degrees Fahrenheit is " + temperature + " degrees Celsius.");
 
 
         }
