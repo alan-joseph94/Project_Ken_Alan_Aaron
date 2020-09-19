b95869e862657c9cfa64bbd442c81e205803b4dd
kenloughery <64917859+kenloughery@users.noreply.github.com>
2020-09-19 13:03:38 -0600

    Fixed spacing to make it more neat

--- Converter.java
+++ Converter.java
@@ -8,10 +8,12 @@ public class Converter {
          * Aaron Joseph
          *
          */
+
         private double celsiusToFahrenheit(double C){
-// TODO: The third student will implement this method
+            // TODO: The third student will implement this method
             return (C*(9/5))+32;
         }
+
         private double fahrenheitToCelsius(double F){
             // TODO: The second student will implement this method
             return ((F-32)*5)/9;
@@ -20,19 +22,20 @@ public class Converter {
         private double kilometersToMiles(double K){
                 return (K * 0.6213711923);
         }
+
         public static void main(String[] args) {
-//TODO: The first student will implement this method.
+                //TODO: The first student will implement this method.
 
-// Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
-        Converter myConverter = new Converter();
-        double temperature = myConverter.celsiusToFahrenheit(180);
-        System.out.println("180 degrees Celsius is " + temperature + " degrees Fahrenheit.");
-// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
-        temperature = myConverter.fahrenheitToCelsius(250);
-        System.out.println("250 degrees Fahrenheit is " + temperature + " degrees Celsius.");
+                // Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
+                Converter myConverter = new Converter();
+                double temperature = myConverter.celsiusToFahrenheit(180);
+                System.out.println("180 degrees Celsius is " + temperature + " degrees Fahrenheit.");
+                // Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
+                temperature = myConverter.fahrenheitToCelsius(250);
+                System.out.println("250 degrees Fahrenheit is " + temperature + " degrees Celsius.");
 
-        double distance = myConverter.kilometersToMiles(30);
-        System.out.println("30 kilometers is " + distance + " miles.");
+                double distance = myConverter.kilometersToMiles(30);
+                System.out.println("30 kilometers is " + distance + " miles.");
       
         }
 }
