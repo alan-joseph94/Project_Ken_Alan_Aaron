ad3ba76e71ac9f4ed7d6c568c4ce5e9d90760513
kenloughery <64917859+kenloughery@users.noreply.github.com>
2020-09-19 11:28:10 -0600

    First student work, completed main

--- Converter.java
+++ Converter.java
@@ -2,7 +2,7 @@ public class Converter {
 
         //Your names go here:
         /*
-         * @Author: Name of the first student
+         * @Author: Kenneth Loughery
          * Alan Joseph
          * Aaron Joseph
          *
@@ -19,8 +19,12 @@ public class Converter {
 //TODO: The first student will implement this method.
 
 // Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
-
+                Converter myConverter = new Converter();
+                double temperature = myConverter.celsiusToFahrenheit(180);
+                System.out.println("180 degrees Celsius is " + temperature + " degrees Fahrenheit.");
 // Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
+                temperature = myConverter.fahrenheitToCelsius(250);
+                System.out.println("250 degrees Fahrenheit is " + temperature + " degrees Celsius.");
 
         }
 }
