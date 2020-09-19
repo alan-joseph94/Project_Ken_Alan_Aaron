edd8116a92a12c9630441aeb2d54814942a1e14b
kenloughery <64917859+kenloughery@users.noreply.github.com>
2020-09-19 12:03:17 -0600

    moved method above main

--- Converter.java
+++ Converter.java
@@ -15,16 +15,20 @@ public class Converter {
             // TODO: The second student will implement this method
             return ((F-32)*5)/9;
         }
+
+        private double kilometersToMiles(double K){
+                return (K * 0.6213711923);
+        }
         public static void main(String[] args) {
 //TODO: The first student will implement this method.
 
 // Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
-
+        Converter myConverter = new Converter();
+        double temperature = myConverter.celsiusToFahrenheit(180);
+        System.out.println("180 degrees Celsius is " + temperature + " degrees Fahrenheit.");
 // Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
+        temperature = myConverter.fahrenheitToCelsius(250);
+        System.out.println("250 degrees Fahrenheit is " + temperature + " degrees Celsius.");
 
-       private double kilometersToMiles(double K){
-            return (K * 0.6213711923);
-            }
 
-        }
 }
